41af1de6b6e37990eb5d0b47957da8703175452b
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-08-17 08:14:08 +0530

    added routes and container

--- src/App.js
+++ src/App.js
@@ -1,56 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Home from "./container/home";
+import Food from "./container/food";
+import Resources from "./container/resources";
+import WallOfHope from "./container/wallOfHope";
+import Login from "./container/login";
+import Dashboard from "./container/dashboard";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/wallOfHope" element={<WallOfHope />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/container/wallOfHope/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WallOfHope = () => {
+  return (
+    <div>
+      <h1>Wall of hope</h1>
+    </div>
+  );
+};
+
+export default WallOfHope;

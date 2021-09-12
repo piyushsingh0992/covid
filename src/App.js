6f304751305d30719147f433185000384de0719a
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-09-12 18:47:56 +0530

    added tabs in dashboard

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <Route path="/login" element={<Login />} /> */}
 
 
-        <Route path="/" element={<Login />} />     
+        <Route path="/" element={<Dashboard />} />     
         
         
          </Routes>
--- src/container/dashboard/index.js
+++ src/container/dashboard/index.js
@@ -1,10 +1,31 @@
 import React from "react";
 import Navbar from "../../components/navbar";
+import { Container } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 const Dashboard = () => {
   return (
     <div>
-      <Navbar/>
-      <h1>Dashboard</h1>
+      <Navbar />
+      <Container maxWidth={750} my={3}>
+        <Tabs variant="enclosed" bg="white" minHeight="90vh">
+          <TabList>
+            <Tab>Settings</Tab>
+            <Tab>Request's</Tab>
+            <Tab>Response</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Container>
     </div>
   );
 };
